44da80e16ec05aaf101277f6053198ea8ec29aa0
Chet Allen <chet.allenv@gmail.com>
2024-02-15 11:36:05 -0500

    Chet

--- Pokemon Project/obj/Debug/net8.0/Pokemon Project.AssemblyInfo.cs
+++ Pokemon Project/obj/Debug/net8.0/Pokemon Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pokemon Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce16790ea9a41b295ce9485616ec3289aa4ba4b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+429431bcbc2ccf23701048fe72216bd727fd8a78")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pokemon Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pokemon Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
